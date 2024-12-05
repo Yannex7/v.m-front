ca0460af2fb7a0d1c0d97ddaf81115d7aec81908
Yannex7 <142844872+Yannex7@users.noreply.github.com>
2024-12-05 21:35:07 +0100

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,26 +1,32 @@
 const encryptedCredentials = CryptoJS.AES.encrypt(JSON.stringify({
     admin: {
         password: "XK92m#pL",
-        isAdmin: true
+        isAdmin: true,
+        stockAdmin: false
     },
     robin: {
         password: "Rb7$kN9v",
-        isAdmin: false
-    },
-    robink: {
-        password: "Rk4@jM8w",
-        isAdmin: false
-    },
-    adrian: {
-        password: "Ad5#nP2x",
-        isAdmin: false
+        isAdmin: false,
+        stockAdmin: true
     },
     andreas: {
         password: "An9$tL6y",
-        isAdmin: false
+        isAdmin: false,
+        stockAdmin: true
+    },
+    robink: {
+        password: "Rk4@jM8w",
+        isAdmin: false,
+        stockAdmin: false
+    },
+    adrian: {
+        password: "Ad5#nP2x",
+        isAdmin: false,
+        stockAdmin: false
     },
     martin: {
         password: "xP91$mV5",
-        isAdmin: false
+        isAdmin: false,
+        stockAdmin: false
     }
 }), "vapeSecretKey").toString();
