f417f748eb56493768ab8470a030e22b89cee5c2
Yannex7 <142844872+Yannex7@users.noreply.github.com>
2025-11-04 12:43:36 +0100

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,6 +1,9 @@
 // Verschlüsselte Zugangsdaten für Inventar Management
 const credentials = {
-    'admin': { password: 'XK92m#pL' }
+    'admin': { password: 'XK92m#pL', role: 'admin' },
+    'Robin': { password: 'robinSecure2024', role: 'creator' },
+    'Tommy': { password: 'tommyPass789', role: 'user' },
+    'Letezia': { password: 'leteziaKey456', role: 'user' }
 };
 
 const encryptedCredentials = CryptoJS.AES.encrypt(
